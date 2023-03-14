6dcbc5951bcfbe753f3de851afa33f48600bf3e4
Julius Gekeler <julius.gekeler@cytolytics.de>
2023-03-14 15:55:58 +0100

    Inital Commit

---
+++ Assets/Scripts/ExperimentManagerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentManager : MonoBehaviour
+{
+
+    public GameObject carPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn the prefab
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
